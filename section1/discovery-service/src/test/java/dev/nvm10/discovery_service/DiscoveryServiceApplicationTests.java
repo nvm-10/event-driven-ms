6fc9e162a463a01d0d4d73e2e323208417a5df1b
ankitsingh <singh10ankit03@gmail.com>
2025-09-25 21:11:03 +0530

    add section1

---
+++ section1/discovery-service/src/test/java/dev/nvm10/discovery_service/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.nvm10.discovery_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
